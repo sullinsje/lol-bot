2ae1376a65453a581a8d3c5fe5071c2d57614254
sullinsje <113549108+sullinsje@users.noreply.github.com>
2025-05-08 10:07:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,10 +23,6 @@ def validateUser(message):
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.command(name="lol")
-async def hello(ctx):
-    await ctx.send("lol")
-
 @bot.event
 async def on_message(message):
     validateUser(message)
@@ -48,21 +44,5 @@ async def on_message(message):
             print("Gif not found...")
             
     await bot.process_commands(message)
-# @bot.command(name="employees")
-# async def employees(ctx):
-#     try:
-#         response = requests.get("http://localhost:8000/employees/")
-#         response.raise_for_status()
-#         employees = response.json()
-
-#         if not employees:
-#             await ctx.send("No employees found")
-#         else:
-#             names = [emp.get("name", "Unknown") for emp in employees]
-#             reply = "Employees:\n" + "\n".join(f"- {name}" for name in names)
-#             await ctx.send(reply)
-          
-#     except requests.RequestException as e:
-#             await ctx.send(f"Failed to get employee(s):\n{e}")
       
 bot.run(TOKEN)
